277c30d6f16752fd261ac94d330bb8feab10e295
ei-lene <eileneheng@gmail.com>
2013-03-01 13:32:44 -0500

    start blogger tutorial from jumpstart labs

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogger::Application.config.secret_token = '235170d473dd6601e32b698dd794df1f108c98ae735bd43b24b4c9f629dd343738361b9048ca9df1d2b4f906e9d4702a2c58aabb6a36ba62fc0b7ab2ca3ad255'
